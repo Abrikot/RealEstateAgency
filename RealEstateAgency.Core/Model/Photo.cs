4665036f2143c4dc6da93d010ed02c3c39df70f6
Maxence Cornaton <cornatonmaxence@gmail.com>
2017-02-28 09:33:59 +0100

    Correction

---
+++ RealEstateAgency.Core/Model/Photo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateAgency.Core.Model
+{
+    class Photo : Parameter
+    {
+        public int id { get; set; }
+        public int estate_id { get; set; }
+        public string title { get; set; }
+        public string photo_base64 { get; set; }
+        public DateTime shooting_date { get; set; }
+    }
+}
