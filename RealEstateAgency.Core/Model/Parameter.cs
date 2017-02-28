6279b8f459a67d99dc6f5748641e03fa92e0717b
Maxence Cornaton <cornatonmaxence@gmail.com>
2017-02-28 08:23:37 +0100

    Ajout des premières sources

---
+++ RealEstateAgency.Core/Model/Parameter.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+using SQLite.Net.Attributes;
+
+namespace RealEstateAgency.Core.Model
+{
+    [Table("parameters"), DataContract]
+    public class Parameter : ViewModels.BaseNotifyPropertyChanged
+    {
+        public const string PARAMETER_KEY_DB_VERSION = "DB_VERSION";
+
+
+        #region Propriétés
+
+        [Column("key"), PrimaryKey, DataMember]
+        public string Key { get { return (string)GetProperty(); } set { SetProperty(value); } }
+
+        [Column("value"), DataMember]
+        public string Value { get { return (string)GetProperty(); } set { SetProperty(value); } }
+
+        #endregion
+
+        public Parameter() : this(false) { }
+        public Parameter(bool synchronizeWithContext = false) : base(synchronizeWithContext) { }
+    }
+}
